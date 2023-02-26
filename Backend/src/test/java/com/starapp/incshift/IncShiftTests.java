100ae706575addf0377428aaa548cf0bcc917031
Hemchandrasuyal <hem.suyal@incedoinc.com>
2023-02-26 09:04:48 +0530

    third commit

--- Backend/src/test/java/com/starapp/incshift/IncShiftTests.java
+++ Backend/src/test/java/com/starapp/incshift/IncShiftTests.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class ShowlistEmployeesToManagerApplicationTests {
+class IncShiftTests {
 
 	@Test
 	void contextLoads() {
